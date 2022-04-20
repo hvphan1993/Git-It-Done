e5b5e4ef75d24ae1e091cee0191b49be86ff9191
hvphan <hvphan1993@gmail.com>
2022-04-19 19:59:18 -0700

    added fetch data as dom elements, added js code to single-repo

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -32,7 +32,7 @@ var getUserRepos = function (user) {
     } else {
       alert("Error: Github User Not Found");
     }
-  });
+  })
 
   .catch(function(error) {
     // Notice this `.catch()` getting chained onto the end of the `.then()` method
