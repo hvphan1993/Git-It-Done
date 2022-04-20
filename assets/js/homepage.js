b88a7a892661e2901f86d081acf263b44706d7ce
hvphan <hvphan1993@gmail.com>
2022-04-19 23:26:09 -0700

    linked pages together with query parameters

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -30,7 +30,7 @@ var getUserRepos = function (user) {
         displayRepos(data, user);
       });
     } else {
-      alert("Error: Github User Not Found");
+      alert("Error: " + response.statusText);
     }
   })
 
@@ -48,11 +48,6 @@ var displayRepos = function (repos, searchTerm) {
     repoContainerEl.textContent = "No repositories found.";
     return;
   }
-  console.log(repos);
-  console.log(searchTerm);
-
-  // clear old content
-  repoContainerEl.textContent = "";
   repoSearchTerm.textContent = searchTerm;
 
   // loop over repos
@@ -60,9 +55,10 @@ var displayRepos = function (repos, searchTerm) {
     // format repo name
     var repoName = repos[i].owner.login + "/" + repos[i].name;
 
-    // create a container for each repo
-    var repoEl = document.createElement("div");
+    // create a link for each repo
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
 
     // create a span element to hold repository name
     var titleEl = document.createElement("span");
